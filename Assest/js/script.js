560be27c761cc9efa8e389996b4e4c3cba520146
Felipedge <fpalmasaldivia@gmail.com>
2022-07-28 23:54:07 -0400

    se termina corrige función agregr tareas

--- Assest/js/script.js
+++ Assest/js/script.js
@@ -5,24 +5,29 @@ const tareas = []
 
 
 btnAgregar.addEventListener("click", () => {
+    if (tareasInput.value === '') {
+        alert('Debe agregar una actividad');
+        return;
+    }
     const nuevaTarea = tareasInput.value
     tareas.push(nuevaTarea)
     tareasInput.value = ""
 
     let html = ""
     for (let tarea of tareas) {
-        html += `<ul>${tarea}</ul>`;
-}
-listadeTareas.innerHTML = html;
+        html += `<ul>${tarea}<button>Realizada</button> <button>Finalizada</button></ul>`;
+    }
+    listadeTareas.innerHTML = html;
 })
 
-btnAgregar.addEventListener("click", () => {
-    nuevaTarea = {id: Date.now(), nombre: invitadoInput.value}
+btnEliminar.addEventListener("click", () => {
+    nuevaTarea = { id: Date.now(), nombre: invitadoInput.value }
     invitados.push(nuevoInvitado)
     invitadoInput.value = ""
     let html = ""
     for (invitado of invitados) {
-    html += `<li>${invitado.nombre}</li>`
+        html += `<li>${invitado.nombre}</li>`
     }
     listaDeInvitados.innerHTML = html;
-    })
+})
+
