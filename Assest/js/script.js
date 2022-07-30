2a9adbee23692913e31a81da228d86e9d4801638
Felipedge <fpalmasaldivia@gmail.com>
2022-07-29 20:18:37 -0400

    se agregan funciones y se mofifica html

--- Assest/js/script.js
+++ Assest/js/script.js
@@ -1,33 +1,71 @@
-const listadeTareas = document.querySelector("#Tareas")
-const tareasInput = document.querySelector("#nuevaTarea")
-const btnAgregar = document.querySelector("#agregarTarea")
-const tareas = []
+const inputTarea = document.querySelector("#tareaInput")
+const totalTareas = document.querySelector("#totalTareas")
+const tareasRealizadas = document.querySelector("#tareasRealizadas")
+const btnAgregar = document.querySelector("#btnAgregar")
+const tablaTareas = document.querySelector("#tareas")
 
+const listaTareas = [
+    { id: 1, descripcion: "Lavarse los dientes", estado: false },
+    { id: 2, descripcion: "Bañarse", estado: false },
+    { id: 3, descripcion: "Tomar desatuno", estado: false }
+]
+let idTarea = listaTareas.length
 
 btnAgregar.addEventListener("click", () => {
-    if (tareasInput.value === '') {
-        alert('Debe agregar una actividad');
-        return;
+    const nuevaTarea = inputTarea.value
+    if (nuevaTarea == "") {
+        alert("Por favor agregar una tarea")
+        return
     }
-    const nuevaTarea = tareasInput.value
-    tareas.push(nuevaTarea)
-    tareasInput.value = ""
-
-    let html = ""
-    for (let tarea of tareas) {
-        html += `<ul>${tarea}<button>Realizada</button> <button>Finalizada</button></ul>`;
-    }
-    listadeTareas.innerHTML = html;
+    idTarea++
+    listaTareas.push({ id: idTarea, descripcion: nuevaTarea, estado: false })
+    inputTarea.value = ""
+    resumenTareas()
 })
+function eliminarTarea(id) {
+    console.log(id)
+    const index = listaTareas.findIndex(tarea => tarea.id == id)
+    listaTareas.splice(index, 1)
+    resumenTareas()
+}
 
-btnEliminar.addEventListener("click", () => {
-    nuevaTarea = { id: Date.now(), nombre: invitadoInput.value }
-    invitados.push(nuevoInvitado)
-    invitadoInput.value = ""
+function cambioEstatus(id) {
+    console.log(id)
+    const index = listaTareas.findIndex(tarea => tarea.id == id)
+    listaTareas[index].estado = !listaTareas[index].estado
+    resumenTareas()
+}
+
+function resumenTareas() {
     let html = ""
-    for (invitado of invitados) {
-        html += `<li>${invitado.nombre}</li>`
-    }
-    listaDeInvitados.innerHTML = html;
-})
+    for (let tarea of listaTareas) {
+        if (tarea.estado == false) {
+            html += `<tr>
+                        <th>${tarea.id}</th>
+                        <td>${tarea.descripcion}</td>
+                        <td ><button type="button"  onclick="cambioEstatus(${tarea.id})"><strong>NO</strong></button></td>
+                        <td ><button type="button"  onclick="eliminarTarea(${tarea.id})"><strong>X</strong></button></td>
+                    </tr>`
 
+        } else {
+            html += `<tr>
+                        <th>${tarea.id}</th>
+                        <td style="text-decoration: line-through">${tarea.descripcion}</td>
+                        <td ><button type="button"  onclick="cambioEstatus(${tarea.id})"><strong>SI</strong></button></td>
+                        <td ><button type="button"  onclick="eliminarTarea(${tarea.id})"><strong>X</strong></button></td>
+                    </tr>`
+        }
+    }
+    tablaTareas.innerHTML = html
+    totalTareas.innerHTML = listaTareas.length
+    tareasRealizada()
+}
+
+function tareasRealizada() {
+    const tareaLista = true
+    const tareasFiltradas = listaTareas.filter((tarea) => tarea.estado === tareaLista);
+    tareasRealizadas.innerHTML = tareasFiltradas.length
+}
+
+
+resumenTareas()
